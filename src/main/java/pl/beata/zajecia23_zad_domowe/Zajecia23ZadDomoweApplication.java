053d215db8bd7db66b7e1ef38bc337ca432ba926
beata <beata_sadek@o2.pl>
2020-06-28 18:03:11 +0200

    first commit

---
+++ src/main/java/pl/beata/zajecia23_zad_domowe/Zajecia23ZadDomoweApplication.java
@@ -0,0 +1,13 @@
+package pl.beata.zajecia23_zad_domowe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zajecia23ZadDomoweApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zajecia23ZadDomoweApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/beata/zajecia23_zad_domowe/Zajecia23ZadDomoweApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.beata.zajecia23_zad_domowe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zajecia23ZadDomoweApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
